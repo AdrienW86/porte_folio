3fdbe337d8cae76300ef737c0b38ea05cfa90e98
Adrien <adrien_weiss@outlook.fr>
2022-11-08 03:33:32 +0100

    responsive

--- src/components/Lightbox/Lightbox.js
+++ src/components/Lightbox/Lightbox.js
@@ -19,28 +19,36 @@ const Lightbox = (props) => {
         function responsiveDisplay(){
             let z;
             if(window.innerWidth < 300)
-                z = 100
+                z = 130
+            else if(window.innerWidth < 350)
+                z = 140
             else if(window.innerWidth < 400)
-                z = 120
+                z = 160
+            else if(window.innerWidth < 450)
+                z = 180
             else if(window.innerWidth < 500)
-                z = 150
+                z = 200
             else if(window.innerWidth < 600)
-                z = 190;
-            else if(window.innerWidth < 820)
+                z = 220;
+            else if(window.innerWidth < 760)
                 z = 250;
+            else if(window.innerWidth < 820)
+                z = 300;
+            else if(window.innerWidth < 1000)
+                z = 315;
             else if(window.innerWidth < 1200)
-                z = 400;
+                z = 370;
             else if(window.innerWidth < 1600)
-                z = 500;
+                z = 450;
             else if(window.innerWidth >= 1600)
-                z = 600;
-            
+                z = 580;
+           
             const span = document.querySelectorAll('.box a');
             span.forEach((element, i)=>{
                 nElements = span.length;
                 const valeur = (360/ nElements) * (i+1);
                 element.style.transform = 'rotateY(' + valeur + 'deg) rotateX(-20deg) translateZ(' + z + 'px)';
-            });
+            })
         }
     
         function clickButtonLeft(){
